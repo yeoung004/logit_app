36bc74de6b0572a81ee0c26e49309dfa44728b85
최영성 <dudtjd4149@gmail.com>
2025-12-29 17:56:44 -0500

    add: custom tab bar with SVG icons and update config
    Implemented a custom TabBar component using SVG icons for each tab and integrated it into the tab layout. Added new SVG assets for active/inactive tab states. Updated app.json with a bundleIdentifier for iOS. Modified package.json scripts for platform-specific runs and added dependencies for lucide-react-native and react-native-svg.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,20 @@
+import { LogitTabBar } from "@/components/ui/TabBar";
 import { Tabs } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
   return (
     <Tabs
+      safeAreaInsets={{ bottom: 0 }}
+      tabBar={(props) => <LogitTabBar {...props} />}
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Tabs.Screen
-        name="note"
-        options={{
-          title: "Note",
-          tabBarIcon: ({ color }) => null,
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => null,
-        }}
-      />
-      <Tabs.Screen
-        name="plan"
-        options={{
-          title: "Plan",
-          tabBarIcon: ({ color }) => null,
-        }}
-      />
-      <Tabs.Screen
-        name="menu"
-        options={{
-          title: "Menu",
-          tabBarIcon: ({ color }) => null,
-        }}
-      />
+      <Tabs.Screen name="note" />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="plan" />
+      <Tabs.Screen name="menu" />
     </Tabs>
   );
 }
